e8389d8cbe18f36532985d3201999e9ab12b886c
minomad <93kymins@gmail.com>
2023-08-26 17:46:02 +0900

    `

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,7 +3,7 @@ import Navigation from './Navigation';
 function Header() {
   return (
     <header>
-      <h1 className="sr-only">주메뉴</h1>
+      <h1 className="sr-only">메뉴</h1>
       <Navigation />
     </header>
   );
