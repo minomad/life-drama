585205eb1446acba312072c86afa24123b8aeff9
minomad <93kymins@gmail.com>
2023-08-27 13:45:15 +0900

    리뷰작성 수정

--- src/pages/ReviewEdit.jsx
+++ src/pages/ReviewEdit.jsx
@@ -44,7 +44,7 @@ function ReviewEdit() {
         <title>리뷰 작성</title>
       </Helmet>
       <Form
-        addStyle={'absolute left-1/2 top-1/3 -translate-x-1/2 -translate-y-1/2 transform'}
+        addStyle={'absolute left-1/2 top-1/3 -translate-x-1/2 -translate-y-1/2 transform min-h-screen'}
         onSubmit={handleReviewSubmit}
       >
         <h2 className="py-5 text-center text-xl font-black">리뷰 작성하기</h2>
@@ -52,7 +52,7 @@ function ReviewEdit() {
           리뷰 내용
         </label>
         <TextArea textareaRef={reviewRef} id={'review'} placeholder={'작품에 대한 감상을 짧게 남겨주세요.'} />
-        <div className="flex gap-5 pt-5 pb-10">
+        <div className="flex gap-5 pt-5">
           <Button type="submit">리뷰 등록</Button>
           <Button hoverColor="hover:bg-rose-500" onClick={handleReviewCancel}>
             취소하기
