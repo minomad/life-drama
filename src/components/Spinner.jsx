d93636a8df26389b07031676aacb54df91b45faa
minomad <93kymins@gmail.com>
2023-08-30 21:40:02 +0900

    스피너 수정

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,6 +1,6 @@
 function Spinner() {
   return (
-    <div className="">
+    <div className="flex justify-center pt-32">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="130"
