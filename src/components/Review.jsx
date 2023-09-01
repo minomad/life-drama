cac6eb7be34efb211430a021f95d4b7698d04d1b
minomad <93kymins@gmail.com>
2023-09-01 16:47:17 +0900

    테스트

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,15 +1,25 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { arrayOf, shape, string } from 'prop-types';
 import Button from '@/components/Button';
+import useStorage from '@/hook/useStorage';
 
 function Review({ id, reviewData }) {
+  const { storageData } = useStorage('pocketbase_auth');
+  const navigate = useNavigate();
+
+  const handleReview = () => {
+    if (!storageData) {
+      alert('로그인 시 가능합니다');
+      return;
+    }
+    navigate(`/review/${id}`);
+  };
+
   return (
     <article className="flex w-full max-w-xl flex-col gap-3 pb-[150px] pt-4 ">
       <div className="flex justify-between pb-2">
         <h2 className="pt-5 text-lg font-semibold">모든 리뷰</h2>
-        <Link to={`/review/${id}`}>
-          <Button>리뷰 작성하기</Button>
-        </Link>
+        <Button onClick={handleReview}>리뷰 작성하기</Button>
       </div>
       {reviewData.map((review) => (
         <div key={review.id} className="w-ful h-28 rounded-lg bg-secondary p-4">
