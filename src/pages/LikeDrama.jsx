e260d047d2017a2fc44eca0e3b097231bd4bc9eb
minomad <93kymins@gmail.com>
2023-09-02 12:45:09 +0900

    test

--- src/pages/LikeDrama.jsx
+++ src/pages/LikeDrama.jsx
@@ -5,7 +5,7 @@ function LikeDrama() {
   const { getListData } = usePocketData('users');
 
   const { data } = useQuery(['users'], () => getListData({ "expand": 'reviews'}));
-  
+
   console.log(data);
   return (
     <div className="flex flex-col items-center">
@@ -14,7 +14,7 @@ function LikeDrama() {
         <ul>
           {data?.map((item, index) => (
             <li key={index}>
-              <h4>{item.expand.like[0].title}</h4>
+              <h4>{item.expand.reviews[0].title}</h4>
             </li>
           ))}
         </ul>
