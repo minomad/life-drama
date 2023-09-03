035c034251f1d618b1834d4fd9882873ae80ce9f
minomad <93kymins@gmail.com>
2023-09-03 18:37:45 +0900

    ㅅ

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,27 +1,44 @@
+
 function Spinner() {
+  
   return (
-    <div className="flex justify-center pt-32">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="130"
-        height="130"
-        preserveAspectRatio="xMidYMid"
-        viewBox="0 0 100 100"
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      style={{margin: 'auto', background: 'transparent'}}
+      width='200'
+      height='200'
+      display='block'
+      preserveAspectRatio='xMidYMid'
+      viewBox='0 0 100 100'
+    >
+      <circle cx='50' cy='50' r='30' fill='none' stroke='#a8b2ff' strokeWidth='10'></circle>
+      <circle
+        cx='50'
+        cy='50'
+        r='30'
+        fill='none'
+        stroke='#5d6fff'
+        strokeLinecap='round'
+        strokeWidth='8'
       >
-        <g>
-          <path fill="none" stroke="#4263eb" strokeWidth="12" d="M50 15a35 35 0 1024.749 10.251"></path>
-          <path fill="#4263eb" d="M49 3v24l12-12L49 3"></path>
-          <animateTransform
-            attributeName="transform"
-            dur="1s"
-            keyTimes="0;1"
-            repeatCount="indefinite"
-            type="rotate"
-            values="0 50 50;360 50 50"
-          ></animateTransform>
-        </g>
-      </svg>
-    </div>
+        <animateTransform
+          attributeName='transform'
+          dur='1s'
+          keyTimes='0;0.5;1'
+          repeatCount='indefinite'
+          type='rotate'
+          values='0 50 50;180 50 50;720 50 50'
+        ></animateTransform>
+        <animate
+          attributeName='stroke-dasharray'
+          dur='1s'
+          keyTimes='0;0.5;1'
+          repeatCount='indefinite'
+          values='18.84955592153876 169.64600329384882;94.2477796076938 94.24777960769377;18.84955592153876 169.64600329384882'
+        ></animate>
+      </circle>
+    </svg>
   );
 }
+
 export default Spinner;
